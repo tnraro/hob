d7465afd2f3d819fd8469750d7edd682df767204
tnraro <deokji53@gmail.com>
2025-03-07 22:11:46 +0900

    fix: 항상 모든 해답을 볼 수 있는 문제 수정

--- src/routes/(root)/problems/[problemId]/+layout.server.ts
+++ src/routes/(root)/problems/[problemId]/+layout.server.ts
@@ -25,11 +25,13 @@ export async function load({ locals, params }) {
   if (problem == null) throw error(404);
 
   const hasExplanation =
-    (await db
-      .select({ id: explanations.id })
-      .from(explanations)
-      .where(and(eq(explanations.problemId, problem.id), eq(explanations.authorId, user.id)))
-      .limit(1)) != null;
+    single(
+      await db
+        .select({ id: explanations.id })
+        .from(explanations)
+        .where(and(eq(explanations.problemId, problem.id), eq(explanations.authorId, user.id)))
+        .limit(1),
+    ) != null;
 
   let allExplanations: (Pick<typeof explanations.$inferSelect, "id" | "authorId"> &
     Pick<typeof users.$inferSelect, "displayName" | "username">)[] = [];
